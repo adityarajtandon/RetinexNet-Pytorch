e5a026378890902b1466b983fe3a2b4cb9a8d40a
Tuyam Pandey <77833276+TuyamPandey@users.noreply.github.com>
2024-10-19 19:48:59 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+import os
+import time
+import random
+
+from PIL import Image
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from utils import *
